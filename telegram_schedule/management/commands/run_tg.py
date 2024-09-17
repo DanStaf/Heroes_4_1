6966f7b1469cd1b973efae55f4ca26629dc7cef9
DanStaf <danstaf@mail.ru>
2024-09-17 23:18:25 +0300

    aiogram removed

--- telegram_schedule/management/commands/run_tg.py
+++ telegram_schedule/management/commands/run_tg.py
@@ -1,9 +1,8 @@
 from django.core.management import BaseCommand
-from telegram_schedule.services import start_polling, good_morning
+from telegram_schedule.services import good_morning
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        # start_polling()
         good_morning()
--- telegram_schedule/services.py
+++ telegram_schedule/services.py
@@ -1,58 +1,9 @@
+import datetime
+
 from config.settings import TG_API_TOKEN
-
-import asyncio
-import logging
-import sys
-
-from aiogram import Bot, Dispatcher, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-
-from apscheduler.schedulers.background import BackgroundScheduler
-
 import telebot
 
-
-TOKEN = TG_API_TOKEN
-
-# All handlers should be attached to the Router (or Dispatcher)
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try! (your message is not supported)")
-
-
-async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    # And the run events dispatching
-    await dp.start_polling(bot)
-
-
-def start_polling():
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
-
-
-##############
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def start_scheduler(my_task, interval_sec):
@@ -65,6 +16,9 @@ def start_scheduler(my_task, interval_sec):
         scheduler.start()
 
 
+############
+
+
 def print_hello():
     print("HELLO")
 
@@ -73,22 +27,15 @@ def start_scheduler_hello():
     start_scheduler(print_hello, 10)
 
 
-def good_morning():
-    text = "Доброе утро!"
-    print(text)
-
-    # bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
+def send_to_tg(chat_id, text):
     bot = telebot.TeleBot(TG_API_TOKEN)
     bot.send_message(1816252417, text)
+    print(text)
+
+
+def good_morning():
+    send_to_tg(1816252417, f"Доброе утро! Сейчас {datetime.datetime.now()}")
 
 
 def start_scheduler_good_morning():
-    scheduler = BackgroundScheduler()
-
-    if not scheduler.get_jobs():
-        scheduler.add_job(print_hello, 'interval', seconds=10)
-
-    if not scheduler.running:
-        scheduler.start()
-
+    start_scheduler(good_morning, 60)

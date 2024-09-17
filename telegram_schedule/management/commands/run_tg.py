7b151969416997d3631b0eb783ac98d1b8dab060
DanStaf <danstaf@mail.ru>
2024-09-18 00:09:22 +0300

    get updates done

--- telegram_schedule/management/commands/run_tg.py
+++ telegram_schedule/management/commands/run_tg.py
@@ -1,8 +1,9 @@
 from django.core.management import BaseCommand
-from telegram_schedule.services import good_morning
+from telegram_schedule.services import get_updates, good_morning
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        good_morning()
+        get_updates()
+        # good_morning()
--- telegram_schedule/services.py
+++ telegram_schedule/services.py
@@ -39,3 +39,62 @@ def good_morning():
 
 def start_scheduler_good_morning():
     start_scheduler(good_morning, 60)
+
+
+def send_poll_train():
+    send_to_tg(1816252417, f"Опрос. Отметьте явку")
+
+
+def send_poll_missed():
+    send_to_tg(1816252417, f"Опрос. Кто предупредил, что пропустит?")
+
+
+def chose_cell():
+    send_to_tg(1816252417, f"В какую локацию завтра едете на тренировку? в Одинцово? кнопки да/нет")
+
+
+def reminder_call_missed():
+    send_to_tg(1816252417, f"Позвонить пропустившим тренировку! список")
+
+
+def reminder_call_new():
+    send_to_tg(1816252417, f"Позвонить новичкам! список")
+
+
+def read_reply():
+    bot = telebot.TeleBot(TG_API_TOKEN)
+    bot.get_chat(1816252417)
+
+
+
+def start_scheduler_mentor():
+
+    scheduler = BackgroundScheduler()
+
+    if not scheduler.get_jobs():
+        scheduler.add_job(chose_cell, 'cron', day_of_week='sat', hour=20, minute=00)
+        scheduler.add_job(send_poll_train, 'cron', day_of_week='sun', hour=8, minute=00)
+        scheduler.add_job(send_poll_missed, 'cron', day_of_week='sun', hour=9, minute=00)
+        scheduler.add_job(reminder_call_missed, 'cron', day_of_week='mon', hour=10, minute=00)
+        scheduler.add_job(reminder_call_new, 'cron', day_of_week='tue', hour=10, minute=00)
+
+    if not scheduler.running:
+        scheduler.start()
+
+
+def start_polling():
+    bot = telebot.TeleBot(TG_API_TOKEN)
+    bot.infinity_polling()
+
+
+def get_updates():
+    # telebot.apihelper.get_updates()
+    bot = telebot.TeleBot(TG_API_TOKEN)
+    res = bot.get_updates(-2)
+    # ответ в формате update_id, message
+    # параметр 869593995 - первый update_id, с которого нужно начинать читать
+
+    [print(
+        f"{item.message.text} from {item.message.chat.id} {item.message.from_user.username}"
+
+    ) for item in res]

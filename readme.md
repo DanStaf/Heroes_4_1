07cd3c15ae458f4065abff8210c4bc9085ac3883
DanStaf <danstaf@mail.ru>
2024-12-18 10:23:40 +0300

    docker added

--- readme.md
+++ readme.md
@@ -15,3 +15,9 @@
 
 - runserver - starts Django web app
 - run_tg - starts TG bot
+
+1) install and start Docker Desktop
+2) Sign out in Docker Desktop
+3) correct chapter 'Docker Engine' in Settings
+4) build image with command for Win: `docker-compose build`
+5) start containers with command for Win: `docker-compose up` or `docker-compose up -d --build`
--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,14 +5,20 @@ from users.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        user = User.objects.create(
-            email='danstaf@mail.ru',
-            tg_id=111111,
-            first_name='Admin',
-            last_name='Admin',
-            is_staff=True,
-            is_superuser=True
-        )
+        if not User.objects.filter(email='danstaf@mail.ru'):
 
-        user.set_password('admin')
-        user.save()
+            user = User.objects.create(
+                email='danstaf@mail.ru',
+                tg_id=111111,
+                first_name='Admin',
+                last_name='Admin',
+                is_staff=True,
+                is_superuser=True
+            )
+
+            user.set_password('admin')
+            user.save()
+
+            print("Superuser created")
+        else:
+            print("Superuser exist")
